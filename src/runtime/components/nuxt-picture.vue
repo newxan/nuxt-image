7684719b4682a37a6bb331fe3ef0b9ea8ca0428f
Oskar Lundh <oskar@startkoden.com>
2022-09-29 12:02:38 +0200

    Fix incorrect picture args

--- src/runtime/components/nuxt-picture.vue
+++ src/runtime/components/nuxt-picture.vue
@@ -5,85 +5,93 @@
       :type="nSources[1].type"
       :srcset="nSources[1].srcset"
       :sizes="nSources[1].sizes"
-    >
+    />
     <img
-      v-bind="{...nImgAttrs, ...imgAttrs}"
+      v-bind="{ ...imgAttrs }"
       :src="nSources[0].src"
       :srcset="nSources[0].srcset"
       :sizes="nSources[0].sizes"
       v-on="$listeners"
-    >
+    />
   </picture>
 </template>
 
 <script lang="ts">
-import type { DefineComponentWithMixin } from '../../types/vue'
-import { imageMixin } from './image.mixin'
+import type { DefineComponentWithMixin } from "../../types/vue";
+import { imageMixin } from "./image.mixin";
 
-import { getFileExtension } from '~image'
+import { getFileExtension } from "~image";
 
-const defineComponent: DefineComponentWithMixin = (opts: any) => opts
+const defineComponent: DefineComponentWithMixin = (opts: any) => opts;
 
 export default defineComponent({
-  name: 'NuxtPicture',
+  name: "NuxtPicture",
   mixins: [imageMixin],
   props: {
     legacyFormat: { type: String, default: null },
-    imgAttrs: { type: Object, default: null }
+    imgAttrs: { type: Object, default: null },
   },
-  head () {
+  head() {
     if (this.preload === true) {
-      const srcKey = typeof this.nSources[1] !== 'undefined' ? 1 : 0
+      const srcKey = typeof this.nSources[1] !== "undefined" ? 1 : 0;
       const link = {
-        rel: 'preload',
-        as: 'image',
-        imagesrcset: this.nSources[srcKey].srcset
-      }
-      if (typeof this.nSources[srcKey].sizes !== 'undefined') {
-        link.imagesizes = this.nSources[srcKey].sizes
+        rel: "preload",
+        as: "image",
+        imagesrcset: this.nSources[srcKey].srcset,
+      };
+      if (typeof this.nSources[srcKey].sizes !== "undefined") {
+        link.imagesizes = this.nSources[srcKey].sizes;
       }
       return {
-        link: [link]
-      }
+        link: [link],
+      };
     }
-    return {}
+    return {};
   },
   computed: {
-    isTransparent (): boolean {
-      return ['png', 'webp', 'gif'].includes(this.originalFormat)
+    isTransparent(): boolean {
+      return ["png", "webp", "gif"].includes(this.originalFormat);
     },
-    originalFormat (): string {
-      return getFileExtension(this.src)
+    originalFormat(): string {
+      return getFileExtension(this.src);
     },
-    nFormat (): string {
+    nFormat(): string {
       if (this.format) {
-        return this.format
+        return this.format;
       }
-      if (this.originalFormat === 'svg') {
-        return 'svg'
+      if (this.originalFormat === "svg") {
+        return "svg";
       }
-      return 'webp'
+      return "webp";
     },
-    nLegacyFormat (): string {
+    nLegacyFormat(): string {
       if (this.legacyFormat) {
-        return this.legacyFormat
+        return this.legacyFormat;
       }
       const formats: Record<string, string> = {
-        webp: this.isTransparent ? 'png' : 'jpeg',
-        svg: 'png'
-      }
-      return formats[this.nFormat] || this.originalFormat
+        webp: this.isTransparent ? "png" : "jpeg",
+        svg: "png",
+      };
+      return formats[this.nFormat] || this.originalFormat;
     },
-    nSources (): Array<{ srcset: string, src?: string, type?: string, sizes?: string }> {
-      if (this.nFormat === 'svg') {
-        return [{
-          srcset: this.src
-        }]
+    nSources(): Array<{
+      srcset: string;
+      src?: string;
+      type?: string;
+      sizes?: string;
+    }> {
+      if (this.nFormat === "svg") {
+        return [
+          {
+            srcset: this.src,
+          },
+        ];
       }
 
-      const formats = this.nLegacyFormat !== this.nFormat
-        ? [this.nLegacyFormat, this.nFormat]
-        : [this.nFormat]
+      const formats =
+        this.nLegacyFormat !== this.nFormat
+          ? [this.nLegacyFormat, this.nFormat]
+          : [this.nFormat];
 
       const sources = formats.map((format) => {
         const { srcset, sizes, src } = this.$img.getSizes(this.src, {
@@ -91,27 +99,27 @@ export default defineComponent({
           sizes: this.sizes || this.$img.options.screens,
           modifiers: {
             ...this.nModifiers,
-            format
-          }
-        })
+            format,
+          },
+        });
 
         return {
           src,
           type: `image/${format}`,
           sizes,
-          srcset
-        }
-      })
+          srcset,
+        };
+      });
 
-      return sources
-    }
+      return sources;
+    },
   },
-  created () {
+  created() {
     if (process.server && process.static) {
       // Force compute sources into ssrContext
       // eslint-disable-next-line no-unused-expressions
-      this.nSources
+      this.nSources;
     }
-  }
-})
+  },
+});
 </script>
